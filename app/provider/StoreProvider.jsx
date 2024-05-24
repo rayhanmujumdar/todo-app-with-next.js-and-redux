182c776ad736df1e863634660a4c4f2709656418
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2024-05-25 02:43:12 +0600

    update todo application ui

--- app/provider/StoreProvider.jsx
+++ app/provider/StoreProvider.jsx
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux';
 
 export default function StoreProvider({ children }) {
     const storeRef = useRef();
+    setInitialLocalStorage();
     if (!storeRef.current) {
-        setInitialLocalStorage();
         // Create the store instance the first time this renders
         storeRef.current = makeStore();
     }
